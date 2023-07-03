1845bb889617bc645560988ee7ee53ca7a0dd926
Achia Rolence <rolence.achia@dme.systems>
2023-07-03 16:39:51 +0100

    Added ingress config to kustomize configuration

--- src/main/java/io/splunk/biz/service/StudentService.java
+++ src/main/java/io/splunk/biz/service/StudentService.java
@@ -14,20 +14,20 @@ public class StudentService {
 
     Logger logger=  LoggerFactory.getLogger(StudentService.class);
 
-    private List<Student> studentList = Arrays.asList(new Student(1, "abc", 23, "Female", "Science"),
-            new Student(2, "bcd", 24, "Male", "Maths"),
-            new Student(3, "wer",25,"Female","English"),
-            new Student(4, "der",26,"Male","History"),
-            new Student(5, "fer",24,"Male","Finance"),
-            new Student(6, "get",20,"Female","Computer"));
+    private final List<Student> studentList = Arrays.asList(new Student(1, "Mary", 23, "Female", "Science"),
+            new Student(2, "John", 24, "Male", "Maths"),
+            new Student(3, "Peter",25,"Female","English"),
+            new Student(4, "Jame",26,"Male","History"),
+            new Student(5, "Andrew",24,"Male","Finance"),
+            new Student(6, "Thomas",20,"Female","Computer"));
 
     public List<Student> getStudents() {
         logger.info("StudentService:getStudents execution started..");
-        List<Student> list = null;
-        list = studentList;
+//        List<Student> list = null;
+//        list = studentList;
         logger.info("StudentService:getStudents response  {} ", Mapper.mapToJsonString(studentList));
         logger.info("StudentService:getStudents execution ended..");
-        return list;
+        return studentList;
     }
 
     public Student getStudentById(int id) {
